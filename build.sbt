3404784cceb625b83a2fef4330645f2f6159d3d8
Your Name <entcrd@gmail.com>
2013-05-24 00:14:18 +0200

    dev

--- build.sbt
+++ build.sbt
@@ -11,3 +11,9 @@ libraryDependencies += "org.apache.commons" % "commons-io" % "1.3.2"
             
 //resolvers += "sonatype-public" at "https://oss.sonatype.org/content/groups/public"
 
+// set the main Scala source directory to be <base>/src
+scalaSource in Compile <<= baseDirectory(_ / "src")
+
+// set the Scala test source directory to be <base>/test
+scalaSource in Test <<= baseDirectory(_ / "test")
+
--- src/findex.scala
+++ src/findex.scala
@@ -1,4 +1,7 @@
 object IndexMaker {
+  object totoshka {
+    val k = 1
+  }
     import java.io.File
     import org.apache.commons.io.IOUtils
     def recursiveListFiles(f: File): Stream[File] = {
@@ -9,8 +12,13 @@ object IndexMaker {
             these.filter(! _.isDirectory).toStream append these.filter(_.isDirectory).flatMap(recursiveListFiles)
     }
     def packTo(buf:java.io.ByteArrayOutputStream,f:File) = {
+      try {
         val fs = new java.io.FileInputStream(f)
         IOUtils.copy(fs,buf)
+        println(buf.size())
+      } catch  {
+        case e:java.io.FileNotFoundException =>
+      }
     }
     def make(dir:String,recursive:Boolean=true) = {
         val files = recursiveListFiles(new File(dir))
